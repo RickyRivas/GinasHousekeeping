7ebf6d4f240b97c82e911d23148558ba9692e088
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-10-26 16:30:59 -0500

    added swiper container

--- scripts/main.js
+++ scripts/main.js
@@ -35,5 +35,20 @@ navOverlay.addEventListener('click', () => {
 //     }
 //     localStorage.setItem('theme', theme)
 // })
-
-
+  var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+      centeredSlides: true,
+        loop: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
